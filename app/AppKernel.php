997ffaf97079c1766506d69e55c544d0d7a81abf
Paul Novikov <paulfcdd@gmail.com>
2017-01-09 08:48:14 +0100

    поправка разницы в бандлах с мастером, добавление файлов переводов

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,8 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new OdinKgBundle\OdinKgBundle(),
-        ];
+			new AppBundle\AppBundle(),
+		];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
