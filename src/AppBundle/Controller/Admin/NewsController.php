8c5456c9dd072025d70e68d567b8d721eb9f6383
Paul Novikov <paulfcdd@gmail.com>
2017-04-03 18:03:34 +0200

    апдейт крудебл сервиса, рефакторинг

--- src/AppBundle/Controller/Admin/NewsController.php
+++ src/AppBundle/Controller/Admin/NewsController.php
@@ -41,10 +41,9 @@ class NewsController extends Controller
      */
     public function addNewsAction(Request $request)
     {
-        /** @var EntityManager $em */
-        $em = $this->getDoctrine()->getManager();
 
-        $form = $this->createForm(NewsType::class)
+        $form = $this
+            ->createForm(NewsType::class)
             ->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -53,45 +52,14 @@ class NewsController extends Controller
                 ->setDateCreated(new \DateTime())
                 ->setAuthor($this->getUser());
 
-            $em->persist($form->getData());
-
-            $em->getConnection()->beginTransaction();
-
-            try {
-                $em->flush();
-
-                $fileName = trim(strtolower(str_replace(' ', '_', ($form['image']->getData()['name']))));
-
-                $uploader = $this->get('app.image_uploader')
-                    ->setDir('news')
-                    ->setFile($form['image']->getData()['file'])
-                    ->setFileName($fileName)
-                    ->upload();
-
-                if ($uploader) {
-                    $image = new Image();
-                    $image
-                        ->setForeignKey($form->getData()->getId())
-                        ->setEntity(News::class)
-                        ->setName($fileName.'.'.$form['image']->getData()['file']->getClientOriginalExtension())
-                        ->setDescription($form['image']->getData()['description'])
-                        ->setAlt(trim($form['image']->getData()['alt']))
-                        ->setTitle(trim($form['image']->getData()['title']));
-                    $em->persist($image);
-                    $em->flush();
-                    $em->getConnection()->commit();
-                } else {
-                    $em->getConnection()->rollback();
-                    var_dump($uploader);
-                    die;
-                }
-            } catch (Exception $exception) {
-                $em->getConnection()->rollback();
-            }
-
+            $crudable = $this
+                ->get('app.crudable')
+                ->setUploadDir('news')
+                ->setPhotos($form['image']->getData())
+                ->setData($form->getData());
 
             return $this->redirectToRoute('admin.news.edit', [
-                'news' => $form->getData()->getId()
+                'news' => $crudable->add()
             ]);
 
         }
@@ -135,14 +103,5 @@ class NewsController extends Controller
      */
     public function deleteNewsAction(Request $request, News $news){
 
-    }
-
-    /**
-     * @param Request $request
-     * @Route("/test/test/test/", name="test.test")
-     */
-    public function testAction(Request $request){
-        var_dump($request->request->all());
-        die;
     }
 }
--- src/AppBundle/Service/Crudable.php
+++ src/AppBundle/Service/Crudable.php
@@ -3,21 +3,204 @@
 namespace AppBundle\Service;
 
 
+use AppBundle\AppBundle;
+use AppBundle\Entity\Image;
+use Doctrine\DBAL\DBALException;
 use Doctrine\ORM\EntityManager;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 class Crudable
 {
     /** @var EntityManager $em */
     protected $em;
 
+    /** @var  ImageUploader $uploader */
+    protected $uploader;
+
+    protected $data;
+
+    protected $object;
+
+    protected $photos;
+
+    protected $uploadDir;
+
+
     /**
      * Crudable constructor.
      * @param EntityManager $entityManager
+     * @param ImageUploader $uploader
      */
-    public function __construct(EntityManager $entityManager)
+    public function __construct(EntityManager $entityManager, ImageUploader $uploader)
     {
         $this->em = $entityManager;
+        $this->uploader = $uploader;
     }
 
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param $data
+     * @return $this
+     */
+    public function setData($data)
+    {
+
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEntity()
+    {
+        return get_class($this->getData());
+    }
+
+    /**
+     * @param null $photos
+     * @return $this
+     */
+    public function setPhotos($photos = null) {
+
+        $this->photos = $photos;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhotos() {
+        return $this->photos;
+    }
+
+    /**
+     * @param $uploadDir | null
+     * @return $this
+     */
+    public function setUploadDir($uploadDir = null) {
+
+        $this->uploadDir = $uploadDir;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUploadDir() {
+
+        return $this->uploadDir;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function add()
+    {
+
+        $tm = null;
+
+        $data = $this->getData();
+
+        $this->em->persist($data);
+
+        if ($this->getPhotos()) {
+            $tm = $this->transactionManager($this->getUploadDir(), $data);
+        }
+
+        if (!$tm) {
+            return false;
+        } else {
+
+            $this->em->flush();
+
+            return $data->getId();
+        }
+    }
+
+    public function deleteToBin() {
+
+    }
+
+    /**
+     * @param $uploadDir
+     * @param $data
+     * @return bool
+     */
+    private function transactionManager($uploadDir, $data) {
+
+        $this->em->persist($data);
+
+        /*BEGIN TRANSACTION IF ENTITY HAS OBJECTS*/
+        $this->em->getConnection()->beginTransaction();
+
+        try {
+
+            $this->em->flush();
+
+            $images = $this->photoUploader($this->getPhotos(), $uploadDir, $data->getId());
+
+            if ($images) {
+                $this->em->getConnection()->commit();
+                return true;
+            } else {
+                $this->em->getConnection()->rollBack();
+                return false;
+            }
+
+
+        } catch (Exception $exception) {
+            return false;
+        }
+    }
+
+    /**
+     * @param $photo
+     * @param $uploadDir
+     * @param $foreignKey
+     * @return bool|string
+     */
+    private function photoUploader($photo, $uploadDir, $foreignKey) {
+
+        $fileName = trim(strtolower(str_replace(' ', '_', ($photo['name']))));
+
+        $uploader = $this->uploader
+            ->setFileName($fileName)
+            ->setFile($photo['file'])
+            ->setDir($uploadDir)
+            ->upload();
+
+        if ($uploader) {
+
+            $image = new Image();
+
+            $image
+                ->setForeignKey($foreignKey)
+                ->setEntity($this->getEntity())
+                ->setName($fileName . '.' . $photo['file']->getClientOriginalExtension())
+                ->setDescription($photo['description'])
+                ->setAlt(trim($photo['alt']))
+                ->setTitle(trim($photo['title']));
+
+            $this->em->persist($image);
+        }
+
+        try {
+            $this->em->flush();
+            return true;
+        } catch (DBALException $exception) {
+            return $exception->getMessage();
+        }
+    }
 
 }
